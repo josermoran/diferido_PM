e281bccc7eedd9e1e4f6da3b381a9c4a11351c2d
josermoran <00135321@uca.edu.sv>
2023-06-10 08:26:19 -0600

    Model

---
+++ 00135321/app/src/main/java/com/example/diferido/data/ActorModel/ActorModel.kt
@@ -0,0 +1,7 @@
+package com.example.diferido.data.ActorModel
+
+data class ActorModel(
+    val name : String,
+    val age: Int,
+    val description : String
+)
---
+++ 00135321/app/src/main/java/com/example/diferido/data/DummyData.kt
@@ -0,0 +1,11 @@
+package com.example.diferido.data
+
+import com.example.diferido.data.ActorModel.ActorModel
+
+var name = "Keanu Reeves"
+var age = 45
+var description = "Ha participad en muchas peliculas"
+
+var actor = mutableListOf(
+    ActorModel(name, age, description)
+)
